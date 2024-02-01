4adfbacc23a520a0e50f7baebd6f560666cd3420
SagiLevy <sagilevy1612@gmail.com>
2024-02-01 21:01:19 +0200

    CHANGING VALUES TO .ENV

--- backend/routes/resetPassword.js
+++ backend/routes/resetPassword.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const twilio = require("twilio");
-// recovery code from twilio = SK7YZLC67S45A2Q6N3S2JQ3L
+// 
 router.post("/", (req, res) => {
   console.log("work");
   function sendSms() {
     const client = new twilio(
-      "AC6cef0ff01d77c9fc6567a3db2fe4bfcb",
-      "e5fe05accf725abfe5f20ccaba54a6ef"
+      ,
+      
     );
     return client.messages
       .create({
         body: "this is massage",
-        from: "+16318562207",
+        from: ,
         to: "+972 54 319 1612",
       })
       .then((massage) => console.log(massage))
