5f9eacb6bca098f6da09cfdba033244826bd8953
SagiLevy <sagilevy1612@gmail.com>
2024-02-01 21:04:22 +0200

    replaced

--- backend/routes/resetPassword.js
+++ backend/routes/resetPassword.js
@@ -1,18 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const twilio = require("twilio");
-// 
+require("dotenv").config();
+
 router.post("/", (req, res) => {
   console.log("work");
   function sendSms() {
     const client = new twilio(
-      ,
-      
+      process.env.TWILIO_AUTH_KEY,
+      process.env.TWILIO_SECONED_ARGUMENT
     );
     return client.messages
       .create({
         body: "this is massage",
-        from: ,
+        from: process.env.TWILIO_FROM_NUMBER,
         to: "+972 54 319 1612",
       })
       .then((massage) => console.log(massage))
